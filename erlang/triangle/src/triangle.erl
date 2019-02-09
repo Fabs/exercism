fe836b9bc73b6ca8488566179204ae5c842531b3
Fabs <fabriciosn@gmail.com>
2019-02-09 23:23:51 +0100

    improve isoceles

--- erlang/triangle/src/triangle.erl
+++ erlang/triangle/src/triangle.erl
@@ -2,14 +2,12 @@
 
 -export([kind/3]).
 
-
 kind(A, B, C) when B > A -> kind(B, A, C);
 kind(A, B, C) when C > B -> kind(A, C, B);
 kind(A, B, C) when A == 0, B == 0, C == 0 -> {error, "all side lengths must be positive"};
 kind(A, B, C) when (B + C) =< A ->
     {error, "side lengths violate triangle inequality"};
 kind(A, A, A) -> equilateral;
-kind(A, A, _) -> isosceles;
-kind(_, A, A) -> isosceles;
+kind(A, B, C) when A =:= B; B =:= C; A =:= C -> isoceles;
 kind(_, _, _) -> scalene.
 
