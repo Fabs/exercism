64b445d0388787338939911a959083c519ac92c3
Fabs <fabriciosn@gmail.com>
2019-02-11 00:24:42 +0100

    roman numerals

--- erlang/roman-numerals/src/roman_numerals.erl
+++ erlang/roman-numerals/src/roman_numerals.erl
@@ -1,9 +1,30 @@
 -module(roman_numerals).
 
 -export([roman/1, fromRoman/1]).
+-define(ROMANS, [
+  {"M", 1000},
+  {"CM", 900},
+  {"D",  500},
+  {"CD", 400},
+  {"C",  100},
+  {"XC",  90},
+  {"L",   50},
+  {"XL",  40},
+  {"X",   10},
+  {"IX",   9},
+  {"V",    5},
+  {"IV",   4},
+  {"I",    1}
+]).
 
-roman(Number) ->
-     romans(Number).
+roman(Number) when Number =< 0 -> {error, "Only positive numbers are allowed"};
+roman(Number) -> roman(Number, "").
+roman(0, RomanAcc) -> RomanAcc;
+roman(Number, RomanAcc) ->
+    Pred = fun({_, N}) -> Number < N end,
+    [{RomanNumber ,NextNumber}| _ ] = lists:dropwhile(Pred, ?ROMANS),
+    roman(Number - NextNumber,RomanAcc ++ RomanNumber).
+
 
 fromRoman(Number) ->
     Reverse = lists:reverse(Number),
@@ -24,17 +45,3 @@ numerals($X) -> 10;
 numerals($V) -> 5;
 numerals($I) -> 1;
 numerals($Z) -> 0.
-
-romans(1000) -> "M";
-romans(900) -> "CM";
-romans(500) -> "D";
-romans(400) -> "CD";
-romans(100) -> "C";
-romans(90) -> "XC";
-romans(50) -> "L";
-romans(40) -> "XL";
-romans(10) -> "X";
-romans(9) -> "IX";
-romans(5) -> "V";
-romans(4) -> "IV";
-romans(1) -> "I".
