e901a7fcdb55648f7f9f5abf99a9b9a85cd2cc23
Fabs <fabriciosn@gmail.com>
2019-02-11 22:16:16 +0100

    Fix conjecture

--- erlang/collatz-conjecture/src/collatz_conjecture.erl
+++ erlang/collatz-conjecture/src/collatz_conjecture.erl
@@ -2,8 +2,9 @@
 
 -export([steps/1]).
 
+steps(N) when N =< 0 -> {error, "Only positive numbers are allowed"};
 steps(N) -> steps(0, N).
+
 steps(S, 1) -> S;
-steps(_S, N) when N =< 0 -> {error, "Only positive numbers are allowed"};
 steps(S, N) when N rem 2 =:= 0 -> steps(S + 1, N div 2);
 steps(S, N) -> steps(S + 1, 3*N + 1).
